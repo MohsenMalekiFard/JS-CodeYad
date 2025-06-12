0921c4a2a786c2a642cf84ca7392a4a8eb2a5c8a
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-12 19:02:01 +0330

    Create Object2.js

---
+++ Object2.js
@@ -0,0 +1,11 @@
+//Object2
+
+function Course(Title,Teacher,Level,Views) {
+    this.Title = Title;
+    this.Teacher = Teacher;
+    this.Level = Level;
+    this.Views = Views;
+}
+
+var Course1 = new Course("JavaScript", "Mohsen", 1, 0);
+var Course2 = new Course("Python", "Mohsen", 2, 11);
