f05a829ffef86ea8c6f0a894bc6f5f8e4b933670
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-12 19:24:25 +0330

    Create 3-DataTypes.js

---
+++ 3-DataTypes.js
@@ -0,0 +1,24 @@
+
+var number=15; //Number
+
+var number2=15;
+
+var Name="Mohammad"; //String
+
+
+var Variable= " CodeYad "+23; //Multiple Variable
+
+var Variable2=  45 - 23 + " CodeYad";
+
+var Variable3= "CodeYad " + 45 + 23;
+
+var Person={firstName:"Mohammad",lastName:"Hashemi"};//Object
+
+var isComplete=false; //Boolean 
+
+var isEqual; 
+
+isEqual = (number==number2);
+
+
+document.write(isEqual);
