a82ee7e3cfa8c302ec764736e217e2ba82b7fa4d
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-12 19:26:31 +0330

    Create SyntaxInJavaScript.js

---
+++ SyntaxInJavaScript.js
@@ -0,0 +1,29 @@
+
+
+
+var a,b,c;
+
+a = 12;
+
+b = 8;
+
+//c= a + b;
+
+//c =a - b;
+
+//c = a * b;
+
+
+ //c = a / b ;
+
+ c=a%b;
+
+
+
+
+
+document.write(c);
+
+//console.log(Name);
+
+//window.alert(Name);
