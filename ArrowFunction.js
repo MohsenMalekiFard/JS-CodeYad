6aeae1729c0a0eed2f527562f2bebb3ce3884f51
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-12 21:15:03 +0330

    Create ArrowFunction.js

---
+++ ArrowFunction.js
@@ -0,0 +1,45 @@
+//function (a,b) {
+//    let c=42;
+//    return a+b;
+//}
+
+(a,b) => {
+    let c = 42;
+    return a +b;
+};
+
+
+let a=4;
+let b=5;
+
+//function(){
+//    return a+b+100;
+//}
+
+() => a+b+100;
+
+
+function(a){
+    return a+100;
+}
+
+(a) => {
+    return a+100;
+}
+
+(a) => a+100;
+
+a => a+100;
+
+
+function bob(a) {
+    let c=14;
+    a-c;
+    return a+100;
+}
+
+let bob=a => {
+    let c = 14;
+    a-c;
+    return a+100;
+}
