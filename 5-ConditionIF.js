e8ea00e8d509bf8e74ea89fdac77d1a9f9e79a4c
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-12 19:21:56 +0330

    Create 5-ConditionIF.js

---
+++ 5-ConditionIF.js
@@ -0,0 +1,21 @@
+
+var a,b;
+var isEqual;
+
+a=50;
+
+b=5;
+
+if(  a == b )
+{
+    isEqual=true; 
+}
+else
+{
+    isEqual=false;
+}
+
+
+a==b ? isEqual=true : isEqual=false;
+
+console.log("the Equal match is:"+isEqual);
