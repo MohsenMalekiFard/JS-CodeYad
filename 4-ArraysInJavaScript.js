0c3f8d71e1a52e10bb6b1c46edd60c95a4108d2f
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-12 19:25:21 +0330

    Create 4-ArraysInJavaScript.js

---
+++ 4-ArraysInJavaScript.js
@@ -0,0 +1,26 @@
+var names=new Array("mohammad","ali","milad","hossein");
+
+
+var MyArray=["ali",23,true,"hossein",24.543656456];
+
+
+var colors = ["red","black","yellow","orange"];
+
+
+//colors.reverse();
+
+//colors.shift();
+
+//colors.unshift("white");
+
+//colors.pop();
+
+//colors.push("white");
+
+//var NewColors=colors.slice();
+
+//var result=colors.indexOf("blue",0);
+
+var arrayString=colors.join("   ");
+
+console.log(arrayString);
