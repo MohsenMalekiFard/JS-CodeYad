1b57047e086a8caf496d48d90167d3d85f2f89c0
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-12 19:15:45 +0330

    Create 14-WhileLoop.js

---
+++ 14-WhileLoop.js
@@ -0,0 +1,33 @@
+const cars=["Benz","BMW","Pride","Peykan","Ford"];
+
+while(i<10)
+{
+    console.log(i);
+}
+
+
+
+while(cars[i])
+{
+    console.log("The Name Car is: " + cars[i]);
+    i++
+}
+
+
+
+
+
+for(let i = 0 ; i<10;i++)
+{
+
+    if(i==5)
+    {
+  continue;
+    }
+
+
+console.log(i);
+
+}
+
+
