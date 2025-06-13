5e4be2369bf4988d58a3b70cb4828164c571a717
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-13 14:53:57 +0330

    Create 71-Abstracting-In-Objects.js

---
+++ 71-Abstracting-In-Objects.js
@@ -0,0 +1,16 @@
+function Car(name, speed) {
+  this.name = name;
+
+   let bodyWeight = 550;
+
+  let calculateSpeed = function () {
+    bodyWeight / 12;
+  };
+
+  this.maxSpeed = function () {
+    calculateSpeed();
+    console.log(speed);
+  };
+}
+
+const pride = new Car("killer machine", 180);
