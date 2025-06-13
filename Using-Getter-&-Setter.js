6aef1d7ca47fcaeb7319db9f2b1aa747f24df60b
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-13 15:14:34 +0330

    Create Using-Getter-&-Setter.js

---
+++ Using-Getter-&-Setter.js
@@ -0,0 +1,33 @@
+function Car(name, speed) {
+  this.name = name;
+
+  let bodyWeight = 550;
+
+  let calculateSpeed = function () {
+    bodyWeight / 12;
+  };
+
+  this.maxSpeed = function () {
+    calculateSpeed();
+    console.log(speed);
+  };
+
+  Object.defineProperty(this,'bodyWeight',{
+    get: function() {
+      return bodyWeight;
+    },
+    set: function(value) {
+      if(value <= 0){
+        throw new Error("Invalid Number");
+      }
+
+      bodyWeight=value+10;
+    }
+
+  })
+
+}
+
+const pride = new Car("killer machine", 180);
+pride.bodyWeight=220;
+console.log(pride.bodyWeight);
