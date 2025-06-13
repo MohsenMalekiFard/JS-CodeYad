ed1ad1f75cb8d21f8d2964a7d2edb45e603479c6
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-13 14:44:14 +0330

    Create Iterating-Objects.js

---
+++ Iterating-Objects.js
@@ -0,0 +1,23 @@
+function Car(name, speed) {
+  this.name = name;
+  this.maxSpeed = function () {
+    console.log(speed);
+  };
+}
+
+
+
+const pride = new Car("killer machine", 180);
+
+for(let key in pride){
+   if(typeof pride[key] !== 'function')
+    console.log(key, typeof pride[key]);
+}
+
+
+const keys=Object.keys(pride);
+console.log(keys);
+
+ 
+if("name" in pride)
+  console.log("name is in pride");
