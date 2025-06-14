4782405e74a65f2e7d7105c89b0d6bf68dfe2156
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-14 21:52:12 +0330

    Create 76-Hoisting.js

---
+++ 76-Hoisting.js
@@ -0,0 +1,17 @@
+//HOISTING
+
+doSomthing();
+doSomthing2();
+
+// function Declaration
+function doSomthing() {}
+
+// function Expression
+const doSomthing2 = function(){}
+
+
+//CLASS Declaration
+class Myclass{}
+
+//CLASS Expression
+const myClass2 = class{};
